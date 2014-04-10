841fd370aeec683f409645d33c47f701f2f7ff69
Tobi Idowu <tidowu93@yahoo.com>
2014-04-10 13:35:20 -0400

    Group Notes Updated

--- model/dog.sql
+++ model/dog.sql
@@ -2,8 +2,7 @@ CREATE TABLE Dog (
   id varchar(32) NOT NULL PRIMARY KEY,
   name varchar(32) NOT NULL,
   age int NOT NULL
-);
 
-INSERT INTO Dog (id, name, age) 
-VALUES
-('1', 'FIDO', '7');
+
+INSERT INTO Dog (id, name, age) VALUES ('1','Fido', '3');
+INSERT INTO Dog (id, name, age) VALUES ('2','Spot', '5');
