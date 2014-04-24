25e5563159087706bf8cce6c11600407d2b05de0
Jared Rodman <jdrodman@comcast.net>
2014-04-24 19:05:47 -0400

    added new-scent-wheel-session form .ejs and .js

---
+++ views/js/new-scent-wheel-session.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+  $('#new-training-form').submit(function(event) {
+    event.preventDefault();
+    submitForm();
+  })
+})
+
+var submitForm = function() {
+  var url = '/addsession';
+  var uuid = guid();
+  $('input[name="uuid"]').val(uuid);
+  $.post(url, $('#new-training-form').serialize(), function(data, status) {
+    if (status == 'success') {
+      window.location.replace('/session');
+    }
+    else {
+      alert('Failed to create new training session. Please try again!');
+    }
+  })
+}
