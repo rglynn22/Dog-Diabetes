b7060a5cfff117a982e48c07b5edd842047a0427
Jared Rodman <jdrodman@comcast.net>
2014-05-03 03:19:27 -0400

    updates

--- views/js/scent-wheel-session-form.js
+++ views/js/scent-wheel-session-form.js
@@ -11,7 +11,7 @@ var submitForm = function() {
   $('input[name="uuid"]').val(uuid);
   $.post(url, $('#new-training-form').serialize(), function(data, status) {
     if (status == 'success') {
-      window.location.replace('/scentwheelsession?id='+uuid);
+      window.location.replace('/scentwheelsession?id=' + uuid +'&dogName=' + dogName);
     }
     else {
       alert('Failed to create new training session. Please try again!');
