908f225f4d6995ccd13e49342bce41528119ef84
Tobi Idowu <tidowu93@yahoo.com>
2014-05-03 16:40:06 -0400

    Session Debug

--- views/js/canister-session-form.js
+++ views/js/canister-session-form.js
@@ -13,7 +13,7 @@ var submitForm = function() {
   
   $.post(url, $('#new-training-form').serialize(), function(data, status) {
     if (status == 'success') {
-      window.location.replace('/canistersession?id=' + uuid +'&dogName=' + dogName);
+      window.location.replace('/canistersession?id=' + uuid +'&dogName=' + $('input[name="dogName"]').val());
     }
     else {
       alert('Failed to create new training session. Please try again!');
