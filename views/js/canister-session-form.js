4c18646f85800e27f52db5f6975900f222cab322
Tobi Idowu <tidowu93@yahoo.com>
2014-05-03 16:11:02 -0400

    Session debug

--- views/js/canister-session-form.js
+++ views/js/canister-session-form.js
@@ -7,9 +7,10 @@ $(document).ready(function() {
 
 var submitForm = function() {
   var url = '/addsession';
-  var uuid = guid();
-  var dogName = $('h2').val();
+  //var uuid = guid();
+  //var dogName = $('h2').val();
   $('input[name="uuid"]').val(uuid);
+  
   $.post(url, $('#new-training-form').serialize(), function(data, status) {
     if (status == 'success') {
       window.location.replace('/canistersession?id=' + uuid +'&dogName=' + dogName);
