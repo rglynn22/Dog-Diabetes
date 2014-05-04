c7a838ed2307be2788f5093f5671eb68894fe3c7
Jared Rodman <jdrodman@comcast.net>
2014-05-04 11:46:44 -0400

    timer fix 2

--- views/js/canister-session.js
+++ views/js/canister-session.js
@@ -35,7 +35,7 @@ var endSession = function() {
   console.log("Ending session");
   var endTime = new Date().getTime();
 	var milliseconds = endTime - startTime;
-	int seconds = Math.floor((milliseconds / 1000) % 60);
+	var seconds = Math.floor((milliseconds / 1000) % 60);
 	var minutes = Math.floor((milliseconds / (1000*60)) % 60);
 	var duration = minutes + " mins, " + seconds + "secs";
 	var url = '/addsessionstats';
