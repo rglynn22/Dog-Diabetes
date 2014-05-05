572e04559b3b5b14096f6cd6bfaeb6de21a6ebc4
britte <lizzy.britton@gmail.com>
2014-05-04 23:01:25 -0400

    wheel session summary string parses

--- views/js/scent-wheel-session-summary.js
+++ views/js/scent-wheel-session-summary.js
@@ -5,21 +5,64 @@ function parseSession(session){
 	} else {
 		var pos1 = 0;
 		var pos2 = 0;
-		var pos3 = 0;
 		var pos4 = 0;
 
+		var totalTrials = 0;
+		var success = 0;
+		var miss = 0;
+		var falseAlert = 0;
+
+		var lastDir = "";
+		var parsedTrials = "";
+
 		var trials = session.split("/");
-		for (t in trials) {
-			var positions = t.split(" ");
-			for (p in positions) {
-				var elems = p.split(",");
+		for (var t=0; t<trials.length; t++) {
+			totalTrials = trials.length;
+			var positions = trials[t].split(" ");
+			parsedTrials += "Trial "+(t+1)+"<br>";
+			for (var p=0; p<positions.length; p++) {
+				var elems = positions[p].split(",");
 				if (elems.length != 3) continue;
 				else {
 					var direction = elems[0];
 					var position = elems[1];
 					var result = elems[2];
+					// Update overall tally
+					switch (position) {
+						case "1": pos1 += parseInt(result); falseAlert ++; break;
+						case "2": pos2 += parseInt(result); falseAlert ++; break;
+						case "4": pos4 += parseInt(result); falseAlert ++; break;
+						case "3": 
+							if (result == "S") success ++;
+							else miss ++;
+					}
+					// Update trial log
+					if (lastDir != direction) { // switch direction
+						if (direction == "c") { parsedTrials+="&rarr;&nbsp;&nbsp;" }
+						else { parsedTrials += "&larr;&nbsp;&nbsp;"}
+					}
+					lastDir = direction;
+					parsedTrials += "Arm "+position+":"+result+"&nbsp;&nbsp;";
 				}
 			}
+			parsedTrials += "<br>";
 		}
+
+		// Produce HTML to represent results
+		// Overall stats
+
+		var html = "";
+		html += "Trials: "+totalTrials+"<br>"+ 
+				"Successes: "+success+"<br>"+
+				"Misses: "+miss+"<br>"+
+				"False Alerts: "+falseAlert+"<br>"+"<br>";
+		html += "<strong>-- Stats By Arm --</strong>"+"<br>"+
+				"Arm 1: "+pos1+" false alerts"+"<br>"+ 
+				"Arm 2: "+pos2+" false alerts"+"<br>"+ 
+				"Arm 3: "+success+"/"+totalTrials+" successes"+"<br>"+ 
+				"Arm 4: "+pos4+" false alerts"+"<br>"+"<br>";
+		html += "<strong>-- Detailed Trials --</strong>"+"<br>"+
+				parsedTrials;
+		$("#stats").append(html);
 	}
 }
